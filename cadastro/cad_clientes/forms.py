d3c09412f15dc6066422d7114ec4cc71b0ba5438
ronaldobergara <bergaraheadbanger@gmail.com>
2020-06-05 00:05:31 -0300

    Melhorando front e personalizando mensagem de erro

--- cadastro/cad_clientes/forms.py
+++ cadastro/cad_clientes/forms.py
@@ -2,13 +2,15 @@ from django import forms
 from django.core.exceptions import ValidationError
 from cadastro.cad_clientes.models import CadCliente
 
+# Personalisar mensagem de erro para todos os campos
+REQUIRED = dict(required=('Campo Obrigatório'))
 
 class CadClienteFormOld(forms.Form):
     cnpj            = forms.CharField(label='CNPJ:')
     razaoSocial     = forms.CharField(label='Razão Social:')   
     nomeFantasia    = forms.CharField(label='Nome Fantasia:')
     cep             = forms.CharField(label='CEP:')
-    endereco        = forms.CharField(label='Endereço:')
+    endereco        = forms.CharField(label='Endereço:', required=True)
     numero          = forms.CharField(label='Numero:')
     uf              = forms.ChoiceField(label='UF:')
     consumidorFinal = forms.BooleanField(label='Consumidor Final', required=False)
@@ -19,3 +21,4 @@ class CadClienteForm(forms.ModelForm):
     class Meta:
         model = CadCliente
         fields = ['cnpj', 'razaoSocial', 'nomeFantasia', 'cep', 'endereco', 'numero', 'uf', 'consumidorFinal']
+        error_messages = {f.name: REQUIRED for f in  CadCliente._meta.fields} # Personalisar mensagem de erro para todos os campos
